4d55310b6120e3d7fd3b2399ec9009141003e5cb
Naman Khare <namankhare329@gmail.com>
2026-09-03 18:30:40 +0530

    Start with javaScript and Selects the all buttons and display

---
+++ script.js
@@ -0,0 +1,24 @@
+// All Buttons Selectors
+const btnAC = document.getElementById("clear");
+const btnDelete = document.getElementById("signDelete");
+const btnPrctge = document.getElementById("percentage");
+const btnDivide = document.getElementById("divide");
+const btnSeven = document.getElementById("seven");
+const btnEight = document.getElementById("eight");
+const btnNine = document.getElementById("nine");
+const btnMltpy = document.getElementById("multiply");
+const btnFour = document.getElementById("four");
+const btnFive = document.getElementById("five");
+const btnSix = document.getElementById("six");
+const btnSubt = document.getElementById("subtract");
+const btnOne = document.getElementById("one");
+const btnTwo = document.getElementById("two");
+const btnThree = document.getElementById("three");
+const btnAdd = document.getElementById("add");
+const btnZero = document.getElementById("zero");
+const btnDecimal1 = document.getElementById("decimal");
+const btnEquals = document.getElementById("equals");
+
+// Display-Result
+const displaySecd = document.getElementById("expression");
+const displayPri = document.getElementById("result");
